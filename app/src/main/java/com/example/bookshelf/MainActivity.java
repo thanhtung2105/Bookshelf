6d257433f17c2afa3f61cf46936466cd1edf6591
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2019-09-29 00:39:33 +0700

    Create MainActivity.java

---
+++ app/src/main/java/com/example/bookshelf/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bookshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container,new ListFragment()).commit();
+    }
+}
